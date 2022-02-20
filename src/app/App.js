fa0b1ea007f6d165682e52f2dc049feb006f7cda
Markian-code <markuskovalko@gmail.com>
2022-02-20 16:00:16 +0200

    add new sort

--- src/app/App.js
+++ src/app/App.js
@@ -1,35 +1,8 @@
-import React, { useState, useEffect } from "react";
-import api from "./api";
+import React from "react";
 import Users from "./components/users";
 
 function App() {
-    const [users, setUsers] = useState(api.users.fetchAll());
-    useEffect(() => {
-        api.users.fetchAll().then((data) => setUsers(data));
-    }, []);
-    const handleDelete = (userId) => {
-        setUsers(users.filter((user) => user._id !== userId));
-    };
-    const handleToggleBookMark = (id) => {
-        const newArray = users.map((user) => {
-            if (user._id === id) {
-                return { ...user, bookmark: !user.bookmark };
-            }
-            return user;
-        });
-        setUsers(newArray);
-    };
-    return (
-        <div>
-            {users && (
-                <Users
-                    onDelete={handleDelete}
-                    onToggleBookMark={handleToggleBookMark}
-                    users={users}
-                />
-            )}
-        </div>
-    );
+    return <Users />;
 }
 
 export default App;
